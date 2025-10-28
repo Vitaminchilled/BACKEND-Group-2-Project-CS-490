ae8f254b577afe62b4a886748578e6121d422bff
Ben <Benporreca04@gmail.com>
2025-10-28 18:51:21 -0400

    Salons to Verify route updated

--- venv/salon.py
+++ venv/salon.py
@@ -30,16 +30,30 @@ def salonsToVerify():
         mysql = current_app.config['MYSQL']
         cursor = mysql.connection.cursor()
         query = """
-            select *
+            select 	s.name,
+                    s.email,
+                    s.phone_number,
+                    s.created_at,
+                    a.address,
+                    a.city,
+                    a.state,
+                    a.postal_code,
+                    a.country,
+                    u.first_name as owner_first_name,
+                    u.last_name as owner_last_name,
+                    u.email as owner_email,
+                    u.phone_number as owner_phone_number,
+                    u.birth_year as owner_birth_year
             from salons s 
             join users u on u.user_id = s.owner_id
-            limit 6
+            join addresses a on a.salon_id = s.salon_id;
         """
         cursor.execute(query)
         rows = cursor.fetchall()
-        columns = [desc[0] for desc in cursor.description]
+        cols = [desc[0] for desc in cursor.description]
         cursor.close()
-        data = [dict(zip(columns, row)) for row in rows]
+        data = [dict(zip(cols, row)) for row in rows]
+
         return jsonify({'salons': data}), 200
     except Exception as e:
         return jsonify({'error': 'Failed to fetch salons', 'details': str(e)}), 500
