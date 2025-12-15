5564b6300d5d7d70ccf83128ac80cd6c17bfaec6
LizethLino <168126140+LizethLino@users.noreply.github.com>
2025-12-15 17:58:43 -0500

    Update appointments.py

--- appointments.py
+++ appointments.py
@@ -266,7 +266,9 @@ def view_appointments():
                     sv.duration_minutes AS service_duration,
                     e.employee_id,
                     CONCAT(e.first_name, ' ', e.last_name) AS employee_name,
-                    e.description AS employee_description
+                    e.description AS employee_description,
+                    a.image_url,
+                    a.image_after_url
                 FROM appointments a
                 JOIN salons s ON a.salon_id = s.salon_id
                 JOIN services sv ON a.service_id = sv.service_id
