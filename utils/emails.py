0088d8dd1f788bb12af2b6a42123975d1d249e30
Vitaminchilled <110579804+Vitaminchilled@users.noreply.github.com>
2025-12-15 22:43:57 -0500

    Update emails.py

--- utils/emails.py
+++ utils/emails.py
@@ -1,7 +1,7 @@
 from flask_mail import Message
 from flask import current_app
 
-def send_email(to, subject, body, html=None):
+'''def send_email(to, subject, body, html=None):
     mail = current_app.extensions.get('mail')
     if not mail:
         raise RuntimeError("Flask-Mail is not initialized. Make sure you called mail.init_app(app).")
@@ -16,4 +16,28 @@ def send_email(to, subject, body, html=None):
     if html:
         msg.html = html
 
-    mail.send(msg)
+    mail.send(msg)'''
+
+def send_email(to, subject, body, html=None):
+    try:
+        mail = current_app.extensions.get('mail')
+        if not mail:
+            log_error("Flask-Mail is not initialized.", session.get("user_id"))
+            return False
+
+        msg = Message(
+            subject=subject,
+            recipients=[to],
+            sender=current_app.config.get("MAIL_USERNAME")
+        )
+        msg.body = body
+
+        if html:
+            msg.html = html
+
+        mail.send(msg)
+        return True
+
+    except Exception as e:
+        log_error(f"Failed to send email: {str(e)}", session.get("user_id"))
+        return False
